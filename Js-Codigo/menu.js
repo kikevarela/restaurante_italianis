bb1863365c0cf7ccaa9598823d426ecc2bcef992
kikevarela <105121642+kikevarela@users.noreply.github.com>
2022-05-07 12:02:41 -0500

    Add files via upload

---
+++ Js-Codigo/menu.js
@@ -0,0 +1,61 @@
+var boton = document.getElementById('boton_menu');
+var nav = document.getElementById('nav');
+boton.addEventListener('click',function () {
+    nav.classList.toggle('mostrar')
+});
+const audio = document.querySelector('audio'),
+    songLength = document.getElementById('SongLength'),
+    currentTime = document.getElementById('CurrentSongTime');
+
+const calculateTime = (secs) =>{
+    const minutes = Math.floor(secs / 60),
+        seconds = Math.floor(secs % 60),
+        returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+        return `${minutes}:${returnedSeconds}`;
+}
+
+const displayDuration = () =>{
+    songLength.innerHTML = calculateTime(audio.duration);
+}
+
+if(audio.readyState > 0){
+    displayDuration();
+    currentTime.innerHTML = calculateTime(audio.currentTime);
+}else{
+    audio.addEventListener('loadedmetadata', () =>{
+        displayDuration();
+    })
+}
+
+audio.ontimeupdate = function(){
+    currentTime.innerHTML = calculateTime(audio.currentTime);
+    setProgress();
+}
+
+function setProgress(){
+    let percentage = (audio.currentTime / audio.duration) * 100;
+    document.querySelector('.progress').style.width = percentage + '%'; 
+}
+
+//Audio Controls
+const playPause = document.getElementById('PlayPause'),
+    plus10 = document.getElementById('Plus10'),
+    back10 = document.getElementById('Back10');
+
+playPause.addEventListener('click', ()=>{
+    if(audio.paused){
+        playPause.src = 'pause.jpg';
+        audio.play();
+    }else{
+        playPause.src = 'play.png';
+        audio.pause();
+    }
+})
+
+plus10.addEventListener('click', ()=>{
+    audio.currentTime +=10;
+})
+
+back10.addEventListener('click', ()=>{
+    audio.currentTime -=10;
+})
